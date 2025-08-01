889bc2072cbc4852c388e58b37b408127fe23c8d
Pedro Gutierres <pedro@imperiumsolucoes.com.br>
2025-08-01 12:17:26 -0300

    Adicionado registro de usuário no front-end e correções no fluxo do back

--- src/backend/auth-api/src/Auth.Application/DTOs/RegisterRequestDto.cs
+++ src/backend/auth-api/src/Auth.Application/DTOs/RegisterRequestDto.cs
@@ -4,7 +4,7 @@ public class RegisterRequestDto
 {
     public string Email { get; set; } = string.Empty;
 
-    public string Password { get; set; } = string.Empty;
+    public string Senha { get; set; } = string.Empty;
 
     public string Nome { get; set; } = string.Empty;
 
--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -11,12 +10,16 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app/app.routes';
 import { environment } from './environments/environment';
 import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
+    provideEnvironmentNgxMask(),
+    provideNativeDateAdapter(),
     provideToastr({
       timeOut: 3000,
       positionClass: 'toast-top-right',
@@ -30,3 +33,4 @@ bootstrapApplication(AppComponent, {
     }),
   ],
 }).catch((err) => console.error(err)); 
+
