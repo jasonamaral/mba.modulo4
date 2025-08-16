e69a8e886442b48f0f3ad8783691bb4968521773
Hugo Domynique <44654088+hugodomynique@users.noreply.github.com>
2025-08-16 16:56:13 -0300

    implementar o ExcluirAulaCommand

---
+++ src/backend/conteudo-api/src/Conteudo.Application/Commands/ExcluirAula/ExcluirAulaCommand.cs
@@ -0,0 +1,17 @@
+﻿using Core.Communication;
+using Core.Messages;
+using MediatR;
+
+namespace Conteudo.Application.Commands.ExcluirAula
+{
+    public class ExcluirAulaCommand : CommandRaiz, IRequest<CommandResult>
+    {
+        public Guid Id { get; }
+
+        public ExcluirAulaCommand(Guid id)
+        {
+            Id = id;
+            DefinirRaizAgregacao(Id);
+        }
+    }
+}
