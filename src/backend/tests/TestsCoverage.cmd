82809382e929efaabba59453bfd08f4bc0f7441b
Jairo Azevedo <jsouza.lp@gmail.com>
2025-09-22 15:07:40 -0300

    Code-coverage

---
+++ src/backend/tests/TestsCoverage.cmd
@@ -0,0 +1,30 @@
+@echo off
+cd..
+setlocal
+echo === Limpando relatórios antigos ===
+if exist .\coverage-report rmdir /s /q .\coverage-report
+
+echo === Limpando testes antigos ===
+set "testesCaminho=.\TestResults"
+for /d /r %testesCaminho% %%d in (TestResults) do (
+    if exist "%%d" (
+        echo Excluindo pasta: %%d
+        rd /s /q "%%d"
+    )
+)
+
+echo === Executando testes Conteudo.UnitTests ===
+dotnet test Conteudo.UnitTests\Conteudo.UnitTests --collect:"XPlat Code Coverage"
+
+echo === Gerando Relatório de Cobertura ===
+reportgenerator -reports:tests\**\coverage.cobertura.xml -targetdir:tests\coverage-report -reporttypes:Html
+
+echo === Abrindo o Relatório ===
+if exist tests\coverage-report\index.html (
+    start tests\coverage-report\index.html
+) else (
+    echo !!! ERRO: Relatório HTML não foi gerado !!!
+)
+
+endlocal
+
